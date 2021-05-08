f1fec0ecbddade2888f880f61fbd1c2582269941
De1m0s114 <yegor1002@gmail.com>
2021-05-08 20:46:43 +0300

    Final laba

--- 60fps/GameLoop.cs
+++ 60fps/GameLoop.cs
@@ -8,7 +8,10 @@ namespace _60fps
 {
     public class GameLoop
     {
+        private Stopwatch _stopwatch;
         private int _fps;
+        private volatile bool _IsRunning;
+        private Thread _thread;
 
         public GameLoop(int fps)
         {
@@ -16,34 +19,46 @@ namespace _60fps
         }
         public void Start(Action action)
         {
-            var thread = new Thread(Work);
-            thread.Start(action);
+            _IsRunning = true;
+            _thread = new Thread(Work);
+            _thread.Start(action);
 
 
         }
         public void Stop()
         {
-
+            _IsRunning = false;
+            _stopwatch.Stop();
+            _thread.Join();
+        }
+        public TimeSpan ElapsetTime
+        {
+            get
+            {
+                return _stopwatch.Elapsed;
+            }
+            
         }
         public void Work(object o)
         {
             var action = o as Action;
-            var stopwatch = Stopwatch.StartNew();
-            var previousTime = stopwatch.ElapsedMilliseconds;
+            
+            _stopwatch = Stopwatch.StartNew();
+            var previousTime = _stopwatch.ElapsedMilliseconds;
 
             var frameDurationMs = 1000f / _fps;
-            while (true)
+            while (_IsRunning)
             {
-                float delta = frameDurationMs - (stopwatch.ElapsedMilliseconds - previousTime);
-                if (stopwatch.ElapsedMilliseconds - previousTime > frameDurationMs)
+                float delta = frameDurationMs - (_stopwatch.ElapsedMilliseconds - previousTime);
+                if (_stopwatch.ElapsedMilliseconds - previousTime > frameDurationMs)
                 {
                     action();
 
-                    previousTime = stopwatch.ElapsedMilliseconds;
+                    previousTime = _stopwatch.ElapsedMilliseconds;
                 }
                 else if (delta > 0)
                 {
-                    Thread.Sleep((int)delta);
+                   //Thread.Sleep((int)delta);
                 }
             }
         }
--- 60fpsTest/UnitTest1.cs
+++ 60fpsTest/UnitTest1.cs
@@ -7,19 +7,34 @@ namespace _60fpsTest
 {
     public class TestFor_60fps
     {
-        public int i = 0;
+        public int i ;
         [SetUp]
-       
+    public void Begin()
+        {
+            i = 0;
+        }
+        
 
         [Test]
         public void TestForFrameDuration()
         {
             var s= new GameLoop(1);
             s.Start(counter);
-            Thread.Sleep(2000);
+            Thread.Sleep(5100);
+            s.Stop();
+            Assert.AreEqual((int)s.ElapsetTime.TotalSeconds, i);
             
-            Assert.AreEqual(2, i);
-            
+        }
+
+        [Test]
+        public void TestForFrameDuration60()
+        {
+            var s = new GameLoop(60);
+            s.Start(counter);
+            Thread.Sleep(1000);
+            s.Stop();
+            Assert.AreEqual(60, i, 2);
+
         }
 
         private void counter()
